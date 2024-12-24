6b88645f1bcd8b66adc79f18983279df5f674541
VathanakSol <vathanaksol1605@gmail.com>
2024-12-24 11:25:26 +0700

    add more config && and new rest controller endpoints

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,18 +1,34 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api/items")
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public List<Map<String, String>> items = new ArrayList<>();
+    private int idCounter = 1;
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> createItem(@RequestBody Map<String, String> newItem){
+        newItem.put("id", String.valueOf(idCounter++));
+        items.add(newItem);
+        Map<String, String> response = new HashMap<>();
+        response.put("message","Item created successfully!!!");
+        response.put("item", newItem.toString());
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @GetMapping("/goodbye")
-    public String goodbye(){
-        return "Just say goodbye";
+    @GetMapping
+    public ResponseEntity<List<Map<String, String>>> getAllItems(){
+        return new ResponseEntity<>(items, HttpStatus.OK);
     }
+
 }
